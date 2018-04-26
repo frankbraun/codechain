b063407a16cb91216e819289397aa343ea1bd282
Frank Braun <frank@cryptogroup.net>
2018-04-26 22:12:51 +0000

    file: add CopyDir() function

--- util/file/file.go
+++ util/file/file.go
@@ -4,6 +4,7 @@ package file
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
@@ -22,6 +23,7 @@ func Exists(filename string) (bool, error) {
 
 // Copy source file src to destination file dst.
 // The destination file must not exist already.
+// The source file must exist already and be a regular file.
 func Copy(src, dst string) error {
 	if dst == "." {
 		dst = filepath.Base(src)
@@ -47,6 +49,10 @@ func Copy(src, dst string) error {
 	if err != nil {
 		return err
 	}
+	// make sure source file is a regular file
+	if !fi.Mode().IsRegular() {
+		return fmt.Errorf("source file '%s' is not a regular file", src)
+	}
 	mode := fi.Mode() & os.ModePerm // only keep standard UNIX permission bits
 	// create destination file
 	d, err := os.OpenFile(dst, os.O_CREATE|os.O_WRONLY, mode)
@@ -57,6 +63,58 @@ func Copy(src, dst string) error {
 	// copy content
 	if _, err := io.Copy(d, s); err != nil {
 		return err
+	}
+	return nil
+}
+
+// CopyDir recursivly copies the source directory src to destination directory
+// dst. The source directory must exist already and only contain regular files
+// and directories. The destination directory must not exist already.
+func CopyDir(src, dst string) error {
+	if dst == "." {
+		dst = filepath.Base(src)
+	}
+	if dst != "." {
+		// make sure destination directory does not exist already
+		exists, err := Exists(dst)
+		if err != nil {
+			return err
+		}
+		if exists {
+			return fmt.Errorf("destination directory '%s' exists already", dst)
+		}
+	}
+	// make sure source directory exists and is a directory
+	fi, err := os.Stat(src)
+	if err != nil {
+		return err
+	}
+	if !fi.IsDir() {
+		return fmt.Errorf("source directory '%s' is not a directory", src)
+	}
+	// make destination directory
+	mode := fi.Mode() & os.ModePerm // only keep standard UNIX permission bits
+	if err := os.MkdirAll(dst, mode); err != nil {
+		return err
+	}
+	// process source directory
+	fis, err := ioutil.ReadDir(src)
+	if err != nil {
+		return err
+	}
+	for _, fi := range fis {
+		s := filepath.Join(src, fi.Name())
+		d := filepath.Join(dst, fi.Name())
+		if fi.IsDir() {
+			// recursion
+			if err := CopyDir(s, d); err != nil {
+				return err
+			}
+		} else {
+			if err := Copy(s, d); err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }

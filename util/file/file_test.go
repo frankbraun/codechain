66644056378b8724c137429c9b4f054f0d5a2545
Frank Braun <frank@cryptogroup.net>
2018-04-26 22:41:37 +0000

    file: add tests

---
+++ util/file/file_test.go
@@ -0,0 +1,34 @@
+package file
+
+import (
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/frankbraun/codechain/tree"
+)
+
+func TestCopyDir(t *testing.T) {
+	tmpdir, err := ioutil.TempDir("", "file_test")
+	if err != nil {
+		t.Fatalf("TempDir() failed: %v", err)
+	}
+	defer os.RemoveAll(tmpdir)
+	dst := filepath.Join(tmpdir, "dst")
+	if err := CopyDir("testdata", dst); err != nil {
+		t.Fatalf("CopyDir() failed: %v", err)
+	}
+	srcHash, err := tree.Hash("testdata", nil)
+	if err != nil {
+		t.Fatalf("tree.Hash(\"testdata\") failed: %v", err)
+	}
+	dstHash, err := tree.Hash(dst, nil)
+	if err != nil {
+		t.Fatalf("tree.Hash(dst) failed: %v", err)
+	}
+	if !bytes.Equal(srcHash, dstHash) {
+		t.Error("srcHash and dstHash differ")
+	}
+}

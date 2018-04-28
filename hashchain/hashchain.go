92cced199e1a9fcc30f3919613d3c77991ca02fb
Frank Braun <frank@cryptogroup.net>
2018-04-28 04:54:39 +0000

    hashchain: add c.DetachedSignature()

--- hashchain/hashchain.go
+++ hashchain/hashchain.go
@@ -239,6 +239,36 @@ func (c *HashChain) Signature(entryHash [32]byte, secKey [64]byte) (string, erro
 	return entry, nil
 }
 
+// DetachedSignature adds a detached signature entry for entryHash signed by
+// pubKey to the hash chain.
+func (c *HashChain) DetachedSignature(entryHash, pubKey [32]byte, signature [64]byte) (string, error) {
+	// TODO: check that entryHash is valid
+	// TODO: make sure secKey is a valid signer
+	// TODO: make sure entryHash is a valid position to sign
+	// Same checks as for Signature()
+	if !ed25519.Verify(pubKey[:], entryHash[:], signature[:]) {
+		return "", fmt.Errorf("signature does not verify")
+	}
+	typeFields := []string{
+		base64.Encode(entryHash[:]),
+		base64.Encode(pubKey[:]),
+		base64.Encode(signature[:]),
+	}
+	prev := c.LastEntryHash()
+	l := &link{
+		previous:   prev[:],
+		datum:      time.Now(),
+		linkType:   signatureType,
+		typeFields: typeFields,
+	}
+	c.chain = append(c.chain, l)
+	entry := l.String()
+	if _, err := fmt.Fprintln(c.fp, entry); err != nil {
+		return "", err
+	}
+	return entry, nil
+}
+
 // LastEntryHash returns the hash of the last entry.
 func (c *HashChain) LastEntryHash() [32]byte {
 	return sha256.Sum256([]byte(c.chain[len(c.chain)-1].String()))

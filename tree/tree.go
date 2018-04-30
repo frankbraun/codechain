380469feb81adf86cd01f094bc72a998d40112b6
Frank Braun <frank@cryptogroup.net>
2018-04-30 20:15:55 +0000

    tree: rework format
    Bring it in line with the capabilities of `git diff` and `git apply` and
    lock it down more.

--- tree/tree.go
+++ tree/tree.go
@@ -15,17 +15,19 @@ import (
 const EmptyHash = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 
 // List returns a list in lexical order of newline separated hashes of all
-// files and directories in the file tree rooted at root, except for the paths
-// in excludePaths. The list includes Unix permission bits (read, write,
-// execute) in octal notation. The deterministic list serves as the basis for
-// a hash of a directory tree.
+// files in the file tree rooted at root, except for the paths in
+// excludePaths. Directories are only implicitely listed (i.e., if they
+// contain files). Entries start with 'f' if it is a regular file (read and
+// write permission for user) and with 'x' if it is an executable (read,
+// write, and executabele for user).
+//
+// The deterministic list serves as the basis for a hash of a directory tree.
 //
 // The directory tree can only contain directories or regular files.
 //
 // Example list:
-//	d 755 bar
-//	f 644 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730 bar/baz.txt
-//	f 644 b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c foo.txt
+//	f 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730 bar/baz.txt
+//	x b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c foo.txt
 func List(root string, excludePaths []string) ([]byte, error) {
 	var b bytes.Buffer
 	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
@@ -55,21 +57,40 @@ func List(root string, excludePaths []string) ([]byte, error) {
 			}
 		}
 		perm := info.Mode().Perm() & os.ModePerm
-		if !info.IsDir() {
-			f, err := os.Open(path)
-			if err != nil {
-				return err
+		if info.IsDir() {
+			if perm&0700 != 0700 {
+				return fmt.Errorf("%s: directory doesn't have all user permissions", path)
 			}
-			defer f.Close()
-			h := sha256.New()
-			if _, err := io.Copy(h, f); err != nil {
-				return err
-			}
-			fmt.Fprintf(&b, "f %3o %x", perm, h.Sum(nil))
-		} else {
-			fmt.Fprintf(&b, "d %3o", perm)
+			return nil
 		}
-		fmt.Fprintf(&b, " %s\n", canonical)
+		var m rune
+		if perm&0100 == 0100 {
+			if perm&0700 != 0700 {
+				return fmt.Errorf("%s: executable is not readable and writable", path)
+			}
+			m = 'x' // executable
+		} else {
+			if perm&0010 > 0 {
+				return fmt.Errorf("%s: regular file is executable for group, but not for user", path)
+			}
+			if perm&0001 > 0 {
+				return fmt.Errorf("%s: regular file is executable for other, but not for user", path)
+			}
+			if perm&0600 != 0600 {
+				return fmt.Errorf("%s: regular file is not readable and writable", path)
+			}
+			m = 'f' // regular file
+		}
+		f, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		h := sha256.New()
+		if _, err := io.Copy(h, f); err != nil {
+			return err
+		}
+		fmt.Fprintf(&b, "%c %x %s\n", m, h.Sum(nil), canonical)
 		return nil
 	})
 	if err != nil {
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -30,12 +30,11 @@ func TestEmpty(t *testing.T) {
 	}
 }
 
-const testdataList = `d 755 bar
-f 644 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730 bar/baz.txt
-f 644 b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c foo.txt
+const testdataList = `f 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730 bar/baz.txt
+f b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c foo.txt
 `
 
-const testdataHash = "9644253f9bd469f4771ff085afac826a17b5690b09c376bee2daf26ecd199d50"
+const testdataHash = "6a311afb95a38e3de2cec9a8566d637198097abe734fef698d8032b80272dc1b"
 
 func TestTestdata(t *testing.T) {
 	l, err := List("testdata", nil)
@@ -54,10 +53,9 @@ func TestTestdata(t *testing.T) {
 	}
 }
 
-const testdataListExclude = `d 755 bar
-f 644 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730 bar/baz.txt
+const testdataListExclude = `f 7d865e959b2466918c9863afca942d0fb89d7c9ac0c99bafc3749504ded97730 bar/baz.txt
 `
-const testdataHashExclude = "c9c1c49eabdc8fdaa5af62ccc924e1b4813ab2ebc876fc582e5cc017c964f2c4"
+const testdataHashExclude = "45e169478843023bb6d8c7fdcca1f1b199404975e2ebb09e76df6819a5dcad10"
 
 func TestTestdataExclude(t *testing.T) {
 	excludePaths := []string{"foo.txt"}

ee3bb878155d2e95a4a23fa5063343c100056b0c
Frank Braun <frank@cryptogroup.net>
2018-05-01 01:25:52 +0000

    linktype: coding style

--- hashchain/linktype/linktype.go
+++ hashchain/linktype/linktype.go
@@ -1,17 +1,20 @@
 // Package linktype defines the different link types of a hash chain.
 package linktype
 
-const (
-	// ChainStart link type.
-	ChainStart = "cstart"
-	// Source link type.
-	Source = "source"
-	// Signature link type.
-	Signature = "signtr"
-	// AddKey link type.
-	AddKey = "addkey"
-	// RemoveKey link type.
-	RemoveKey = "remkey"
-	// SignatureControl link type.
-	SignatureControl = "sigctl"
-)
+// ChainStart link type.
+const ChainStart = "cstart"
+
+// Source link type.
+const Source = "source"
+
+// Signature link type.
+const Signature = "signtr"
+
+// AddKey link type.
+const AddKey = "addkey"
+
+// RemoveKey link type.
+const RemoveKey = "remkey"
+
+// SignatureControl link type.
+const SignatureControl = "sigctl"

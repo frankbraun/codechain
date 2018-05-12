ba82879561a01b40ec0ef2f468740c8e77b21eca
Frank Braun <frank@cryptogroup.net>
2018-05-12 00:44:35 +0000

    file: extend tests

--- util/file/file_test.go
+++ util/file/file_test.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/frankbraun/codechain/internal/def"
 	"github.com/frankbraun/codechain/tree"
 	"github.com/frankbraun/codechain/util/file"
 )
@@ -18,7 +19,8 @@ func TestCopyDir(t *testing.T) {
 	}
 	defer os.RemoveAll(tmpdir)
 	dst := filepath.Join(tmpdir, "dst")
-	if err := file.CopyDir("testdata", dst); err != nil {
+	err = file.CopyDir("testdata", dst)
+	if err != nil {
 		t.Fatalf("CopyDir() failed: %v", err)
 	}
 	srcHash, err := tree.Hash("testdata", nil)
@@ -31,5 +33,62 @@ func TestCopyDir(t *testing.T) {
 	}
 	if !bytes.Equal(srcHash[:], dstHash[:]) {
 		t.Error("srcHash and dstHash differ")
+	}
+
+	// make dir which should be excluded
+	err = os.Mkdir(filepath.Join(dst, def.CodechainDir), 0755)
+	if err != nil {
+		t.Fatalf("os.Mkdir() failed: %v", err)
+	}
+	dst2 := filepath.Join(tmpdir, "dst2")
+	err = file.CopyDirExclude(dst, dst2, def.ExcludePaths)
+	if err != nil {
+		t.Fatalf("CopyDirExclude() failed: %v", err)
+	}
+	dst2Hash, err := tree.Hash(dst2, nil)
+	if err != nil {
+		t.Fatalf("tree.Hash(dst2) failed: %v", err)
+	}
+	if !bytes.Equal(srcHash[:], dst2Hash[:]) {
+		t.Error("srcHash and dst2Hash differ")
+	}
+
+	// make sure dir wasn't copied
+	exists, err := file.Exists(filepath.Join(dst2, def.CodechainDir))
+	if err != nil {
+		t.Fatalf("Exists() failed: %v", err)
+	}
+	if exists {
+		t.Error("exists should be false")
+	}
+}
+
+func TestIsBinaryTrue(t *testing.T) {
+	isBinary, err := file.IsBinary(filepath.Join("testdata", "gopher.png"))
+	if err != nil {
+		t.Fatalf("IsBinary() failed: %v", err)
+	}
+	if !isBinary {
+		t.Error("isBinary should be true")
+	}
+}
+
+func TestIsBinaryFalse(t *testing.T) {
+	isBinary, err := file.IsBinary(filepath.Join("testdata", "foo.txt"))
+	if err != nil {
+		t.Fatalf("IsBinary() failed: %v", err)
+	}
+	if isBinary {
+		t.Error("isBinary should be false")
+	}
+}
+
+func TestExistsTrue(t *testing.T) {
+	exists, err := file.Exists(filepath.Join("testdata", "foo.txt"))
+	if err != nil {
+		t.Fatalf("Exists() failed: %v", err)
+	}
+	if !exists {
+		t.Error("exists should be true")
 	}
 }

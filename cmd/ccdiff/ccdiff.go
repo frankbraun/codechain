092628ed7cdfb7b67fd5994e0ce026f2dc7021d9
Frank Braun <frank@cryptogroup.net>
2018-09-27 00:18:48 +0000

    ccdiff: add option -v (logs to stderr)

--- cmd/ccdiff/ccdiff.go
+++ cmd/ccdiff/ccdiff.go
@@ -2,12 +2,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
 	"github.com/frankbraun/codechain/internal/def"
 	"github.com/frankbraun/codechain/patchfile"
 	"github.com/frankbraun/codechain/util"
+	"github.com/frankbraun/codechain/util/log"
 )
 
 func diff(a, b string) error {
@@ -15,15 +17,23 @@ func diff(a, b string) error {
 }
 
 func usage() {
-	fmt.Fprintf(os.Stderr, "usage: %s tree_a tree_b\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "Usage: %s tree_a tree_b\n", os.Args[0])
+	fmt.Fprintf(os.Stderr, "Caluates a patch between two directory trees and print it to stdout.\n")
+	flag.PrintDefaults()
 	os.Exit(2)
 }
 
 func main() {
-	if len(os.Args) != 3 {
+	verbose := flag.Bool("v", false, "Be verbose (on stderr)")
+	flag.Usage = usage
+	flag.Parse()
+	if *verbose {
+		log.Std = log.NewStd(os.Stderr)
+	}
+	if flag.NArg() != 2 {
 		usage()
 	}
-	if err := diff(os.Args[1], os.Args[2]); err != nil {
+	if err := diff(flag.Arg(0), flag.Arg(1)); err != nil {
 		util.Fatal(err)
 	}
 }

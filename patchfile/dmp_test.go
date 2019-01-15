9ea3a463e0b7460c938b85a0c3f685cf5e2d555b
Frank Braun <frank@cryptogroup.net>
2019-01-15 07:49:42 +0000

    patchfile: encode tables.go to circumvent problem
    We want to be able to publish this file with the old Codechain version
    which cannot handle it.

--- patchfile/dmp_test.go
+++ patchfile/dmp_test.go
@@ -4,14 +4,21 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"testing"
+
+	"github.com/frankbraun/codechain/internal/ascii85"
 )
 
 // Tests that show where github.com/sergi/go-diff/diffmatchpatch fails.
 func TestDMP(t *testing.T) {
-	filename := filepath.Join("testdata", "dmp", "tables.go")
-	b, err := ioutil.ReadFile(filename)
+	filename := filepath.Join("testdata", "dmp", "tables.go.bin")
+	enc, err := ioutil.ReadFile(filename)
 	if err != nil {
 		t.Fatalf("ioutil.ReadFile() failed: %v", err)
+	}
+	// tables.go.bin -> tables.go
+	b, err := ascii85.Decode(enc)
+	if err != nil {
+		t.Fatalf("ascii85.Decode() failed: %v", err)
 	}
 	// patchApply fails
 	text := string(b)

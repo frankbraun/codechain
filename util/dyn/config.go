39d71e146a507cc43ac282f2f2765f9b1f8e0e9d
Frank Braun <frank@cryptogroup.net>
2019-01-25 12:34:11 +0000

    dyn: add Config type

---
+++ util/dyn/config.go
@@ -0,0 +1,52 @@
+package dyn
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/frankbraun/codechain/util/file"
+)
+
+// ConfigFilename defines the default filename for Dyn Managed DNS API Config
+// files.
+const ConfigFilename = "dyn.json"
+
+// A Config for the Dyn Managed DNS API.
+type Config struct {
+	CustomerName string // customer_name
+	UserName     string // user_name
+	Password     string // password
+}
+
+// ReadConfig from filename.
+func ReadConfig(filename string) (*Config, error) {
+	var c Config
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(data, &c); err != nil {
+		return nil, err
+	}
+	return &c, nil
+}
+
+// Write config to filename.
+func (c *Config) Write(filename string) error {
+	exists, err := file.Exists(filename)
+	if err != nil {
+		return err
+	}
+	if exists {
+		return fmt.Errorf("configuration file '%s' exists already", filename)
+	}
+	jsn, err := json.MarshalIndent(c, "", "  ")
+	if err != nil {
+		return err
+	}
+	if err := ioutil.WriteFile(filename, jsn, 0644); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ util/dyn/config_test.go
@@ -0,0 +1,36 @@
+package dyn
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+func TestConfig(t *testing.T) {
+	tmpdir, err := ioutil.TempDir("", "config_test")
+	if err != nil {
+		t.Fatalf("TempDir() failed: %v", err)
+	}
+	defer os.RemoveAll(tmpdir)
+
+	c := &Config{"foo", "bar", "baz"}
+
+	// Write()
+	filename := filepath.Join(tmpdir, ConfigFilename)
+	if err := c.Write(filename); err != nil {
+		t.Fatalf("c.Write() failed: %v", err)
+	}
+
+	// ReadConfig()
+	rc, err := ReadConfig(filename)
+	if err != nil {
+		t.Fatalf("ReadConfig(%s) failed: %v", filename, err)
+	}
+
+	// compare
+	if !reflect.DeepEqual(c, rc) {
+		t.Error("Read config doesn't equal written config")
+	}
+}

ec1c4a3e96ff3e625c667c0ab96944b6d3bd22d9
Frank Braun <frank@cryptogroup.net>
2019-12-20 16:24:32 +0000

    new option `secpkg install -p`

--- secpkg/command/install.go
+++ secpkg/command/install.go
@@ -5,19 +5,44 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/frankbraun/codechain/secpkg"
+	"github.com/frankbraun/codechain/util/homedir"
 	"github.com/frankbraun/codechain/util/log"
 )
+
+func install(pkgFlag bool, name string) error {
+	ctx := context.Background()
+	if pkgFlag {
+		// make sure codechain is actually installed
+		if _, err := secpkg.CheckUpdate(ctx, "codechain"); err != nil {
+			if err == secpkg.ErrNotInstalled {
+				fmt.Fprintf(os.Stderr, "you must install codechain via `secpkg install` in order to use option -p\n")
+			}
+			return err
+		}
+		securePackageDir := filepath.Join(homedir.SecPkg(), "pkgs", "codechain",
+			"src", "packages")
+		name = filepath.Join(securePackageDir, name+".secpkg")
+	}
+	// 1. Parse .secpkg file and validate it.
+	pkg, err := secpkg.Load(name)
+	if err != nil {
+		return err
+	}
+	return pkg.Install(ctx)
+}
 
 // Install implements the secpkg 'install' command.
 func Install(argv0 string, args ...string) error {
 	fs := flag.NewFlagSet(argv0, flag.ContinueOnError)
 	fs.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s project.secpkg\n", argv0)
+		fmt.Fprintf(os.Stderr, "Usage: %s [-p] project.secpkg\n", argv0)
 		fmt.Fprintf(os.Stderr, "Download, verify, and install package defined by project.secpkg.\n")
 		fs.PrintDefaults()
 	}
+	pkgFlag := fs.Bool("p", false, "Install secure package file of given name distributed by Codechain")
 	verbose := fs.Bool("v", false, "Be verbose")
 	if err := fs.Parse(args); err != nil {
 		return err
@@ -32,10 +57,5 @@ func Install(argv0 string, args ...string) error {
 	if err := secpkg.UpToDate("codechain"); err != nil {
 		return err
 	}
-	// 1. Parse .secpkg file and validate it.
-	pkg, err := secpkg.Load(fs.Arg(0))
-	if err != nil {
-		return err
-	}
-	return pkg.Install(context.Background())
+	return install(*pkgFlag, fs.Arg(0))
 }
